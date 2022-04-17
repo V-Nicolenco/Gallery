edfea25895188f7ecf20f48af647811b0e685e7d
Vladyslav Nikolenko <v.nikolenko@spd-ukraine.com>
2022-04-17 12:30:42 +0300

    Changed default success url

--- api/src/main/java/mother/hackers/gallery/configuration/SecurityConfiguration.java
+++ api/src/main/java/mother/hackers/gallery/configuration/SecurityConfiguration.java
@@ -38,14 +38,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS,"*").permitAll()//allow CORS option calls
-                .mvcMatchers("/login", "/registration", "/web-api/login", "/web-api/registration", "/").permitAll()
+                .mvcMatchers("/login", "/registration", "/web-api/login", "/web-api/registration").permitAll()
                 .anyRequest().authenticated();
 
         http.cors().configurationSource(request -> configureCors());
 
         http.formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/profiles/me", true)
+                .defaultSuccessUrl("/", true)
                 .and()
                 .logout()
                 .logoutSuccessUrl("/login")
